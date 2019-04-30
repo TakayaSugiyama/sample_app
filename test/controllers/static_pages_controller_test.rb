d47133222baa19b7ccd3b9cdae74e350f09505be
takaya sugiyama <s.takaya1027@gmail.com>
2019-04-30 09:01:38 +0900

    3.5まで

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
+
+  test "should get root" do
+    get static_pages_home_url
+    assert_response :success
+  end
+ 
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
@@ -15,11 +21,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "Home | #{@base_title}"
   end
 
-  test "should get help" do
+  test "sholud get help" do
     get static_pages_help_url
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
-  end
+ end
+
 
   test "sholud get about" do
      get static_pages_about_url
