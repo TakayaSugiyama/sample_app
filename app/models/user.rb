8f3714220f86837bb87a4e559d9929465f5b623d
takaya sugiyama <s.takaya1027@gmail.com>
2019-05-02 22:14:58 +0900

    mail address test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     validates :name,{presence: true,length:{maximum: 50}}
-    validates :email,{presence: true,length:{maximum : 225}}
+    before_save {email.downcase!}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email,{presence: true, length:{maximum: 225},  format: { with: VALID_EMAIL_REGEX },
+                      uniqueness: {case_sensitive: false}}
 end
