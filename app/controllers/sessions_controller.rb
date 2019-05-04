fb35de193634ec7e42c20d4c3fad2f155b2e7714
takaya sugiyama <s.takaya1027@gmail.com>
2019-05-04 21:29:27 +0900

    change header layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,18 @@
 class SessionsController < ApplicationController
+  include SessionsHelper
+  helper_method :log_in,:current_user,:logged_in?
+
   def new
     @user = User.new
   end
   
   def create
-    @user = User.find_by(email: params[:session][:email].downcase)
-    if @user &&  @user.authenticate(params[:session][:password])
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user &&  user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
-      session[:user_id] = @user.id
-      redirect_to("/users/#{@user.id}")
+      #session[:user_id] = user.id
+      log_in(user)
+      redirect_to("/users/#{user.id}")
     else
       flash.now[:danger] = 'Invalid email/password combination' 
       render("sessions/new")
