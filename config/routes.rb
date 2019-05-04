f55f9e71bf9ec3f014b4e6658da2a6e10c0fd35d
takaya sugiyama <s.takaya1027@gmail.com>
2019-05-04 14:14:41 +0900

    routeの設定とtest書いた

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
   get "/about"        => 'static_pages#about'
   get "/signup"  => "users#new"
   resources :users
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
